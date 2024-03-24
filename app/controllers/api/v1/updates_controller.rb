dcb28d83aa1241943f25adc28676645432986048
silky <noonsilk+-noonsilk@gmail.com>
2024-03-24 09:26:11 +0000

    fix up tags to only do one query

--- app/controllers/api/v1/updates_controller.rb
+++ app/controllers/api/v1/updates_controller.rb
@@ -5,20 +5,16 @@ module Api
     class UpdatesController < ApplicationController
       include ActionView::Helpers::DateHelper
       def all
-        updates = WorkUpdate.includes(:person).all.order(created_at: :desc)
+        updates = WorkUpdate.includes(:person, :tags).all.order(created_at: :desc)
 
         result = updates.map do |u|
-          # Note, per result we do a query. This is not ideal, but okay for
-          # now and can be refactored later.
-          tags = TagWorkUpdate.includes(:tag).where(work_update_id: u.id)
-
           # NOTE: There's potential for a lot of duplication in the results
           # here; bringing down the same image for the same person, the same
           # colour for the same tag, etc. But let's just note it and worry
           # about it later :)
           { 'person' => { 'name' => u.person.name, 'image' => u.person.image },
             'content' => u.content,
-            'tags' => tags.map { |t| { 'name' => t.tag.name, 'colour' => t.tag.colour } },
+            'tags' => u.tags.map { |tag| { 'name' => tag.name, 'colour' => tag.colour } },
             'created_ago' => "#{time_ago_in_words(u.created_at)} ago",
             'created_at' => u.created_at }
         end
--- app/models/work_update.rb
+++ app/models/work_update.rb
@@ -2,4 +2,6 @@
 
 class WorkUpdate < ApplicationRecord
   belongs_to :person
+  has_many :tag_work_updates
+  has_many :tags, through: :tag_work_updates
 end
